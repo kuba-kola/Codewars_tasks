4b3a7b62c6ff3d781201d5630786bc32da2ed046
Yakau Trafimovich <5243212@gmail.com>
2022-01-27 19:24:47 +0300

    27.01

---
+++ Alphabetti Spaghetti.js
@@ -0,0 +1,8 @@
+// TASK:
+// Description:
+// You will be given a string of letters. The string must be returned in alphabetical order.
+
+
+function alphabetti(str) {
+    return str.split``.sort((a,b)=>a.toLowerCase()>b.toLowerCase()).join``
+  }
