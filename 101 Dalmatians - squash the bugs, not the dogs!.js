43a0c35c87f9704d11ce70c44d64fe639407862a
Yakau Trafimovich <5243212@gmail.com>
2022-04-05 17:57:21 +0200

    refactoring

--- 101 Dalmatians - squash the bugs, not the dogs!.js
+++ 101 Dalmatians - squash the bugs, not the dogs!.js
@@ -3,15 +3,14 @@
 // By repairing the function provided, you will find out exactly how you should respond, depending on the number of dogs he has.
 // The number of dogs will always be a number and there will always be at least 1 dog.
 
-function howManyDalmatians(number){
-  
-    if (number <= 10) {
-      return "Hardly any"
-    } else if (number <= 50) {
-      return "More than a handful!"
-    } else if (number === 101) {
-      return "101 DALMATIANS!!!"
-    } else {
-      return "Woah that's a lot of dogs!" 
-    }
+function howManyDalmatians(number) {  
+  if (number <= 10) {
+    return 'Hardly any';
+  } else if (number <= 50) {
+    return 'More than a handful!';
+  } else if (number === 101) {
+    return '101 DALMATIANS!!!';
+  } else {
+    return `Woah that's a lot of dogs!`;
   }
+}
